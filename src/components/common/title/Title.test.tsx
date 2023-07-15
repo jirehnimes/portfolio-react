d785c80cc69c5271aec130ed6096475bf8ecc4d2
Jireh Nimes <jnimes@malvedaholdings.com>
2023-07-16 00:51:26 +0800

    Updated Title component and unit test

--- src/components/common/title/Title.test.tsx
+++ src/components/common/title/Title.test.tsx
@@ -3,77 +3,232 @@ import '@testing-library/jest-dom';
 
 import Title from './Title';
 
+const TITLE_CONTAINER_CLASS = 'title-container';
 const TITLE_CLASS = 'title';
+const SUBTITLE_CLASS = 'subtitle';
 const DEFAULT_CHILDREN = 'Hello World!';
 
+const assertTitleContainer = (
+  container: HTMLElement,
+): HTMLDivElement | null => {
+  const titleContainer = container.querySelector('div');
+
+  expect(titleContainer).toBeInTheDocument();
+  expect(titleContainer).toHaveClass(TITLE_CONTAINER_CLASS);
+
+  return titleContainer;
+};
+
 describe('Title', () => {
-  it('renders default', () => {
+  it('renders title default', () => {
     const { container } = render(<Title>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h3');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h3');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-3');
   });
 
-  it('renders size 1', () => {
+  it('renders title size 1', () => {
     const { container } = render(<Title size={1}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h1');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h1');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-1');
   });
 
-  it('renders size 2', () => {
+  it('renders title size 2', () => {
     const { container } = render(<Title size={2}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h2');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h2');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-2');
   });
 
-  it('renders size 3', () => {
+  it('renders title size 3', () => {
     const { container } = render(<Title size={3}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h3');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h3');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-3');
   });
 
-  it('renders size 4', () => {
+  it('renders title size 4', () => {
     const { container } = render(<Title size={4}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h4');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h4');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-4');
   });
 
-  it('renders size 5', () => {
+  it('renders title size 5', () => {
     const { container } = render(<Title size={5}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h5');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h5');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
     expect(title).toHaveClass('is-5');
   });
 
-  it('renders size 6', () => {
+  it('renders title size 6', () => {
     const { container } = render(<Title size={6}>{DEFAULT_CHILDREN}</Title>);
 
-    const title = container.querySelector('h6');
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h6');
 
     expect(title).toBeInTheDocument();
     expect(title).toHaveClass(TITLE_CLASS);
+    expect(title).toHaveClass('is-6');
+  });
+
+  // SUBTITLE
+
+  it('renders subtitle default', () => {
+    const { container } = render(<Title subtitle>{DEFAULT_CHILDREN}</Title>);
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h5');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-5');
+  });
+
+  it('renders subtitle size 1', () => {
+    const { container } = render(
+      <Title
+        size={1}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h1');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-1');
+  });
+
+  it('renders subtitle size 2', () => {
+    const { container } = render(
+      <Title
+        size={2}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h2');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-2');
+  });
+
+  it('renders subtitle size 3', () => {
+    const { container } = render(
+      <Title
+        size={3}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h3');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-3');
+  });
+
+  it('renders subtitle size 4', () => {
+    const { container } = render(
+      <Title
+        size={4}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h4');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-4');
+  });
+
+  it('renders subtitle size 5', () => {
+    const { container } = render(
+      <Title
+        size={5}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h5');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
+    expect(title).toHaveClass('is-5');
+  });
+
+  it('renders subtitle size 6', () => {
+    const { container } = render(
+      <Title
+        size={6}
+        subtitle
+      >
+        {DEFAULT_CHILDREN}
+      </Title>,
+    );
+
+    const titleContainer = assertTitleContainer(container);
+
+    const title = titleContainer!.querySelector('h6');
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveClass(SUBTITLE_CLASS);
     expect(title).toHaveClass('is-6');
   });
 });
--- src/components/common/title/Title.tsx
+++ src/components/common/title/Title.tsx
@@ -5,16 +5,26 @@ import { ReactNode } from 'react';
 type TitlePropsType = {
   children: ReactNode;
   size?: number;
+  subtitle?: boolean;
 };
 
-const Title = ({ size = 3, children }: TitlePropsType) => {
-  if (size === 6) return <h6 className='title is-6'>{children}</h6>;
-  if (size === 5) return <h5 className='title is-5'>{children}</h5>;
-  if (size === 4) return <h4 className='title is-4'>{children}</h4>;
-  if (size === 3) return <h3 className='title is-3'>{children}</h3>;
-  if (size === 2) return <h2 className='title is-2'>{children}</h2>;
+const Title = ({
+  // prettier-ignore
+  children,
+  size,
+  subtitle,
+}: TitlePropsType) => {
+  const type = !!subtitle === true ? 'subtitle' : 'title';
+  const _size = !!size === true ? size : !!subtitle === true ? 5 : 3;
 
-  return <h1 className='title is-1'>{children}</h1>;
+  let component = <h1 className={`${type} is-1`}>{children}</h1>;
+  if (_size === 6) component = <h6 className={`${type} is-6`}>{children}</h6>;
+  if (_size === 5) component = <h5 className={`${type} is-5`}>{children}</h5>;
+  if (_size === 4) component = <h4 className={`${type} is-4`}>{children}</h4>;
+  if (_size === 3) component = <h3 className={`${type} is-3`}>{children}</h3>;
+  if (_size === 2) component = <h2 className={`${type} is-2`}>{children}</h2>;
+
+  return <div className='title-container'>{component}</div>;
 };
 
 export default Title;
